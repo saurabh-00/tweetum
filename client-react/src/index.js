8b0132f31acad5beea3a10b0cccd3e08dadb88cc
Saurabh <saurabhnegi.snn@gmail.com>
2024-12-20 18:44:45 +0530

    Navbar

--- client-react/src/index.js
+++ client-react/src/index.js
@@ -3,9 +3,9 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { Provider } from 'react-redux';
-import store from 'store';
 import { PersistGate } from 'redux-persist/integration/react';
 import { persistStore } from 'redux-persist';
+import store from 'store';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
